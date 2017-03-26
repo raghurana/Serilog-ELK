b1bc206143dd1efa94a41a48a6049cc175c57bb3
Raghu Rana <raghurana@gmail.com>
2017-03-26 16:45:12 +1100

    Serilog Console App.

---
+++ ConsoleApplication1/LoggerFactory.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Serilog;
+
+namespace ConsoleApplication1
+{
+    public static class LoggerFactory
+    {
+        private const string OutputFormat = "[{Timestamp:yyyy-MM-dd HH:mm:ss}-{MachineName}-{Level:w3}] {Message}{NewLine}{Exception}";
+
+        private static readonly Lazy<ILogger> LoggerInstance = new Lazy<ILogger>(Builder);
+
+        public static ILogger Logger => LoggerInstance.Value;
+
+        private static ILogger Builder()
+        {
+            return
+                new LoggerConfiguration()
+                    .Enrich.WithMachineName()
+                    .WriteTo.Trace(outputTemplate: OutputFormat)
+                    .WriteTo.LiterateConsole(outputTemplate: OutputFormat)
+                    .CreateLogger();
+        }
+    }
+}
--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -1,15 +1,31 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ConsoleApplication1
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
+            var position = new { Latitude = 25, Longitude = 134 };
+            var elapsedMs = 34;
+
+            LoggerFactory.Logger.Information("Processed {@Position} in {Elapsed} ms.", position, elapsedMs);
+
+            try
+            {
+               BadMethod();
+            }
+            catch (Exception ex)
+            {
+                LoggerFactory.Logger.Error(ex, "Murphy's Law");
+            }
+
+            Console.ReadLine();
+        }
+
+        static void BadMethod()
+        {
+            throw new Exception("Something went wrong !!");
         }
     }
 }
